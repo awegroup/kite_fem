fe27ca1015e2df3cf93fdc3a9cd9322f75ce638a
Patrick Roeleveld <roeleveldpatrick@gmail.com>
2025-05-29 12:56:48 +0200

    Plot update

--- Cantilever/Results/plot.py
+++ Cantilever/Results/plot.py
@@ -35,9 +35,9 @@ Horizontal_disp = np.array(PSM2D["Horizontal_disp"])
 Beam_angle = np.array(PSM2D["Beam_angle"])
 
 L_ratios = np.unique_values(L_ratio)
-fig1, ax1 = plt.subplots(figsize=(6,6))
-fig2, ax2 = plt.subplots(figsize=(6,6))
-fig3, ax3 = plt.subplots(figsize=(6,6))
+fig1, ax1 = plt.subplots(figsize=(4,4))
+fig2, ax2 = plt.subplots(figsize=(4,4))
+fig3, ax3 = plt.subplots(figsize=(4,4))
 
 
 for i in range(len(L_ratios)):
@@ -61,24 +61,27 @@ ax3.plot(FEM_timoshenko5["Beam_angle"],FEM_timoshenko5["Load_param"], linestyle=
 
 
 
-ax1.legend()
-ax1.set_xlabel(f"Non-Dimensional Deflection "+r"$w/L$ [-]")
-ax1.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ [-]")
+ax1.legend(fontsize="small")
+ax1.set_xlabel(f"Non-Dimensional Deflection "+r"$w/L$ (-)")
+ax1.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ (-)")
 ax1.set_xlim(0,1)
 ax1.set_ylim(0,10)
 ax1.grid()
-ax2.legend()
-ax2.set_xlabel(f"Non-Dimensional Deflection "+r"$u/L$ [-]")
-ax2.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ [-]")
+fig1.tight_layout()
+ax2.legend(fontsize="small")
+ax2.set_xlabel(f"Non-Dimensional Deflection "+r"$u/L$ (-)")
+ax2.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ (-)")
 ax2.set_xlim(0,1)
 ax2.set_ylim(0,10)
 ax2.grid()
-ax3.legend()
-ax3.set_xlabel(f"Tip Angle "+r"$\theta_0$ [rad]")
-ax3.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ [-]")
+fig2.tight_layout()
+ax3.legend(fontsize="small")
+ax3.set_xlabel(f"Tip Angle "+r"$\theta_0$ (rad)")
+ax3.set_ylabel(f"Load Parameter "+r"$\frac{P L^2}{EI}$ (-)")
 ax3.set_xlim(0,1.5)
 ax3.set_ylim(0,10)
 ax3.grid()
+fig3.tight_layout()
 
 
 
@@ -89,7 +92,7 @@ Vertical_disp = PSM3D["Vertical_disp"]
 Horizontal_disp = PSM3D["Horizontal_disp"]
 Beam_angle = PSM3D["Beam_angle"]
 # Plotting 3D PSM Cantilever force angle sweep vs target values
-fig4, ax4 = plt.subplots(figsize=(12,6))
+fig4, ax4 = plt.subplots(figsize=(8,4))
 ax4.plot(Force_angle, Vertical_disp, color=colors[0],label=r"$w/L$")
 ax4.plot([0,90],[0.46326,0.46326], linestyle="--",color=colors[0])
 ax4.plot(Force_angle, Horizontal_disp,color=colors[1], label=r"$u/L$")
@@ -97,11 +100,12 @@ ax4.plot([0,90],[0.13981,0.13981], linestyle="--",color=colors[1])
 ax4.plot(Force_angle, Beam_angle,color=colors[2], label=r"$\theta_0$")
 ax4.plot([0,90],[0.72876,0.72876], linestyle="--",color=colors[2])
 ax4.grid()
-ax4.set_xlabel(r"Force angle $\alpha$ [°]")
-ax4.set_ylabel(r"Non-dimensional deflection $w/L$, $u/L$ [-] & angle $\theta_0$ [rad]")
+ax4.set_xlabel(r"Force angle $\alpha$ (°)")
+ax4.set_ylabel(r"$\mathrm{Non\text{-}dimensional\ deflection}\ w/L,\ u/L\ [-]$" "\n" r"$\mathrm{angle}\ \theta_0\ (rad)$")
 ax4.set_xlim(0,90)
 ax4.set_ylim(0,1)
-ax4.legend()
+ax4.legend(fontsize="small")
+fig4.tight_layout()
 
 
 fig1.savefig(os.path.join(script_dir, '..', 'Figures', 'Cantilever_Vertical_disp.pdf'))
