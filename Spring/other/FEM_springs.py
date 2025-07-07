39d7fd3e42ac4c01c3c6019fe1cc023dd9c234a6
Patrick Roeleveld <roeleveldpatrick@gmail.com>
2025-07-07 15:53:43 +0200

    Added convergence parameters

--- Spring/other/FEM_springs.py
+++ Spring/other/FEM_springs.py
@@ -14,11 +14,17 @@ def smart_inverse(A, threshold=1e10):
 
 # Define spring properties
 k = 1  # Stiffness of the first spring in N/m
-F = 10   # Force in N
-l0 = 1
+F = 1  # Force in N
+l0 = 0
+
+# convergence parameters, can be tuned for convergence
+limit = 1.0  # Maximum displacement limit in m
+relaxation = 1  # Initial under-relaxation factor for displacements
+offset = 0.01  # Introduce small offset in initial displacement of DOF's to avoid zero displacements
+
 # Node coordinates
 ncoords = np.array([[0.0, 0.0, 0.0],  # Node at x = 0 m
-                    [1, 2, 0.0], # Node at x = 1 m
+                    [1, 0.0, 0.0], # Node at x = 1 m
                     [2, 0, 0.0]]) 
 
 nids = np.array([0, 1, 2])  # Node IDs
@@ -52,13 +58,6 @@ for n1, n2 in zip(n1s, n2s):
         spring.c1 = DOF * pos1
         spring.c2 = DOF * pos2
         spring.kxe =  k # Spring stiffness
-        # xi = ncoords_init[n2*3] - ncoords_init[n1*3] 
-        # xj = ncoords_init[n2*3+1] - ncoords_init[n1*3+1]
-        # xk = ncoords_init[n2*3+2] - ncoords_init[n1*3+2]
-        # tmp = (xi**2 + xj**2 + xk**2)**0.5
-        # xi /= tmp
-        # xj /= tmp
-        # xk /= tmp
         spring.update_rotation_matrix(ncoords_init)
         spring.update_KC0(KC0r, KC0c, KC0v)
         springs.append(spring)
@@ -82,36 +81,48 @@ fu = f[bu]
 KC0uu = KC0uu.toarray()
 
 
-
-print("KC0uu",KC0)
-
 KC0uu_inv = smart_inverse(KC0uu)
 
 uu = KC0uu_inv @ fu  # Solve for displacements
+
+# limit the displacements to a maximum of 1.0
+uu = np.clip(uu, -limit, limit)
+
+# implement small displacement to each dof if that dof is initially 0
+uu = np.where(uu == 0, offset, uu)
+
+
+
 u = np.zeros(N)
 u[bu] = uu  # Assign displacements to free DOFs
 
 # Define tolerance and maximum iterations
-tolerance = 1e-5
+tolerance = 1e-3
 max_iterations = 100
 
-# Initialize displacement vector
-u = np.zeros(N)
-uu = np.zeros_like(fu)
 
 xyz = np.zeros_like(u, dtype=bool)
 xyz[0::DOF] = xyz[1::DOF] = xyz[2::DOF] = True  
-ncoords_current = ncoords_init.copy()
+ncoords_current = ncoords_init.copy()+u[xyz]  
 
 print("Initial Node Coordinates:", ncoords_init)
+print("Current Node Coordinates:", ncoords_current)
 
+residual_norm = 0
 # Iterative solver
 for iteration in range(max_iterations):
+    # Update reduced stiffness matrix
+    KC0v *= 0
+    for spring in springs:
+        spring.update_rotation_matrix(ncoords_current)
+        spring.update_KC0(KC0r, KC0c, KC0v)
+        
+    KC0 = coo_matrix((KC0v, (KC0r, KC0c)), shape=(N, N)).tocsc()
+    KC0uu = KC0[bu, :][:, bu].toarray() 
+    
     #compute internal forces
     fi = np.zeros(N, dtype=DOUBLE)
     for spring in springs:
-        c1 = spring.c1
-        c2 = spring.c2
         n1 = spring.n1
         n2 = spring.n2
         xi = ncoords_current[n2*3] - ncoords_current[n1*3]
@@ -131,39 +142,29 @@ for iteration in range(max_iterations):
     residual = fu - fi[bu]
 
     # Check if the residual is below the tolerance
+    
+    residual_norm_prev = residual_norm
     residual_norm = np.linalg.norm(residual)
     if residual_norm < tolerance:
-        print("Converged!")
+        print("Converged in", iteration + 1, "iterations!")
         break
+    if residual_norm_prev < residual_norm and iteration > 0:
+        print("Diverging, relaxing solution.")
+        relaxation *= 0.5
+        
 
-    # Update displacements 
-    uu += (smart_inverse(KC0uu) @ residual )
-    
+    # Update displacements limit deltas of 1m
+    uu += np.clip((smart_inverse(KC0uu) @ residual)*relaxation,-limit,limit)
     u = np.zeros(N)
     u[bu] = uu  # Assign displacements to free DOFs
 
     ncoords_current = ncoords_init + u[xyz]
     print("Current Node Coordinates:", ncoords_current)
 
-    KC0v *= 0
-    for spring in springs:
-        spring.update_rotation_matrix(ncoords_current)
-        spring.update_KC0(KC0r, KC0c, KC0v)
-        
-    KC0 = coo_matrix((KC0v, (KC0r, KC0c)), shape=(N, N)).tocsc()
-    KC0uu = KC0[bu, :][:, bu].toarray()  # Update reduced stiffness matrix
+    
     
 
     
-print("residual:", residual)
-print("uu:", uu)
-print("fu:", fu)
-print( "KC0uu @ uu",KC0uu @ uu)
-print("KC0uu:", KC0uu)
-# Assign displacements to the global displacement vector
-
-
-print(fi[bu])
 
 y = ncoords_current[4]
 x = 1
@@ -173,30 +174,3 @@ print("Force in one spring:", F)
 Fy = F * (y / hyp)  # Vertical component of the force
 print("Vertical component of the force:", Fy)
 
-# fi = np.zeros(N, dtype=DOUBLE)
-# for spring in springs:
-#     print("spring:", spring)
-#     print("-------------------------------------------")
-#     c1 = spring.c1
-#     c2 = spring.c2
-#     n1 = spring.n1
-#     n2 = spring.n2
-#     xi = ncoords_current[n1*3] - ncoords_current[n1*3]
-#     xj = ncoords_current[n2*3+1] - ncoords_current[n1*3+1]
-#     xk = ncoords_current[n2*3+2] - ncoords_current[n1*3+2]
-#     l = (xi**2 + xj**2 + xk**2)**(1/2)
-#     fi_local = np.array([spring.kxe * (l - l0),0,0])  # Spring force
-#     R = np.array([[spring.r11,spring.r12,spring.r13],[spring.r21,spring.r22,spring.r23],[spring.r31,spring.r32,spring.r33]])
-#     fi_global = R @ fi_local  # Transform to global coordinates
-#     fi_global = np.append(fi_global,[0,0,0])
-    
-#     bu1 = bu[spring.c1:spring.c1+DOF]
-#     bu2 = bu[spring.c2:spring.c2+DOF]
-#     print("bu1:", bu1)
-#     print("bu2:", bu2)
-#     print("fi_global:", fi_global)
-#     print("c1:", c1, "c2:", c2)
-#     fi[c1:c2] -= fi_global*bu1
-#     fi[c2:c2+DOF] += fi_global*bu2
-#     print("fi:", fi)
-#     print("-------------------------------------------")
