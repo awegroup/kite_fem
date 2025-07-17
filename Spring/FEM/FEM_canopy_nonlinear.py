0fcda6eb9395b51b72a1a2a9dcbaff06b4ae49fb
Patrick Roeleveld <roeleveldpatrick@gmail.com>
2025-07-17 14:33:20 +0200

    added information

--- Spring/FEM/FEM_canopy_nonlinear.py
+++ Spring/FEM/FEM_canopy_nonlinear.py
@@ -3,8 +3,8 @@ import numpy as np
 from FEMStructure import FEM_structure
 
 # Define initial conditions and connectivity matrix
-initial_conditions = [[[0.0, 0.0, 0.0], [0, 0, 0], 1, True],[[1.0, 0.1, 0.0], [0, 0, 0], 1, False],[[2.0, 0.0, 0.0], [0, 0, 0], 1, True]]
-connectivity_matrix = [[0, 1, 1, 1, 0, 'default'], [1, 2, 1, 1, 0, 'default']]
+initial_conditions = [[[0.0, 0.0, 0.0], [0, 0, 0], 1, True],[[1.0, 0.1, 0.0], [0, 0, 0], 1, False],[[2.0, 0.0, 0.0], [0, 0, 0], 1, True]] #[position, velocity (not used), mass (not used),fixed]
+connectivity_matrix = [[0, 1, 1, 1, 0, 'default'], [1, 2, 1, 1, 0, 'default']] #[node1, node2, spring constant, damping constant (not used), initial length, springtype]
 fe = np.zeros(len(initial_conditions)*6, dtype=float)
 fe[7] = 10  # Force applied at node 2 in y-direction
 
