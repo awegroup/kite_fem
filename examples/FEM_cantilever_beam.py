dac56e34baf00123d205c030c4af221ec0d9cc9d
Patrick Roeleveld <roeleveldpatrick@gmail.com>
2025-09-22 11:08:43 +0200

    printing info

--- examples/FEM_cantilever_beam.py
+++ examples/FEM_cantilever_beam.py
@@ -4,12 +4,10 @@ from kite_fem.FEMStructure import FEM_structure
 
 initital_conditions = []
 length = 10 #m
-elements = 3
+elements = 5
 
 for i in range(elements+1):
     initital_conditions.append([[i*length/elements, 0.0, 0.0], [0, 0, 0], 1, True if i==0 else False])
-
-initital_conditions.append([[length, -1.0, 0.0], [0, 0, 0], 1, False])
 
 load_param = 1#
 E = 210e5 # Pa
@@ -23,11 +21,7 @@ beam_matrix = []
 for i in range(elements):
     beam_matrix.append([i, i+1, E, A, I])
 
-spring_matrix = []
-
-spring_matrix.append([3, 4, 10, 0, 1, "default"])
-
-steel_beam = FEM_structure(initital_conditions, beam_matrix=beam_matrix,spring_matrix=spring_matrix)
+steel_beam = FEM_structure(initital_conditions, beam_matrix=beam_matrix)
 
 tip_load = load_param*E*I/(length**2)
 fe = np.zeros(steel_beam.N)
@@ -38,9 +32,9 @@ ax,fig = steel_beam.plot_3D(color= "blue")
 
 steel_beam.solve(        fe=fe,
         max_iterations=1000,
-        tolerance=1e-1,
-        step_limit=0.5,
-        relax_init=0.25,
+        tolerance=0.1,
+        step_limit=2,
+        relax_init=1,
         relax_update=0.95,
         k_update=1,
         I_stiffness=25
@@ -52,16 +46,9 @@ ax.set_xlim([0, 10])
 ax.set_ylim([-10, 0])
 ax.set_zlim([-5, 5])
 
-
+print("Tip displacements")
 print("load_param", load_param)
 print("w/L", -steel_beam.coords_rotations_current[1::6][-1]/length)
 print("u/L", 1-steel_beam.coords_rotations_current[0::6][-1]/length)
 print("theta", -steel_beam.coords_rotations_current[-1])
 plt.show()
-# k = steel_beam.KC0
-# bu = steel_beam.bu
-# k = k[bu, :][:, bu]
-# f = fe[bu]
-# u = np.zeros(steel_beam.N)
-# u[bu] = np.linalg.solve(k.toarray(), f)
-# print(u)
