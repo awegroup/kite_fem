709e93eea6d6cd298f1970383fa2fd48af5e37fb
PatrickRoel <roeleveldpatrick@gmail.com>
2026-01-24 15:29:26 +0100

    Fake aerodynamic loading for detailed kite model example

--- examples/FEM_detailed_kite.py
+++ examples/FEM_detailed_kite.py
@@ -79,19 +79,35 @@ all_sections.sort(key=lambda section: section[0])
 
 #Fake aerodynamic load
 fe = np.zeros(kite.N)
+f_aero = 700 #N (in z direction)
+coords = kite.coords_init.reshape(-1,3)
 for section in all_sections:
-    for i in section:
-        fe[i*6+2] += 1
+    for n,i in enumerate(section):
+        x_pos = coords[i][0]
+        if n < 3:
+            fe[i*6+2] += 1.2 + (x_pos+0.5)/8
+        if n ==3:
+            fe[i*6+2] += 3
+        else:
+            fe[i*6+2] += 1 - (x_pos+0.5)/4
+        y_pos = coords[i][1]
+        fe[i*6+1] += y_pos/6
+scale = f_aero/np.sum(fe[2::6])
+fe *= scale
+
+# gravity
+fe[2::6] -= m_arr*9.81
+
 
 
 print("DOF",np.sum(kite.bc))
 print("Beam",np.size(kite.beam_elements))
 print("Springs",np.size(kite.spring_elements))
-
+print("Mass",np.sum(m_arr))
 ax1,fig1 = plot_structure(kite,plot_nodes=False,fe=fe,plot_external_forces=True,linewidth = [1,0.75,1,3.5],plot_node_numbers=False)
 ax2,fig2 = plot_structure(kite, plot_nodes=False,plot_displacements=False,solver="spsolve",e_colors = ['black', 'black', 'black', 'black'],linewidth = [1,0.75,1,3.5],plot_2d=True,plot_2d_plane="yz")
 ax3,fig3 = plot_structure_with_strain(kite)
-kite.solve(fe=fe, max_iterations=1000, tolerance=0.01, step_limit=.005, relax_init=.25, relax_min=0.00, relax_update=0.998, k_update=1,I_stiffness=15)
+kite.solve(fe=fe, max_iterations=20000, tolerance=0.01, step_limit=.005, relax_init=.25, relax_min=0.00, relax_update=0.9998, k_update=1,I_stiffness=15)
 
 # plot_cross_sections(kite,all_sections)
 ax4,fig4 = plot_structure(kite,fe_magnitude=1.5, plot_residual_forces=False,plot_external_forces=True,plot_nodes=False,plot_displacements=True,solver="spsolve",linewidth = [1,0.75,1,3.5])
