cfda8464ad33ff969ef785420e6d223dfe5bbb7a
PatrickRoel <roeleveldpatrick@gmail.com>
2026-03-09 12:17:01 +0100

    Updated saddle case

--- examples/FEM_saddle.py
+++ examples/FEM_saddle.py
@@ -6,12 +6,13 @@ import numpy as np
 #Input file taken from https://github.com/awegroup/Particle_System_Simulator/blob/main/examples/saddle_form/saddle_form_input.py
 
 # grid discretization
-grid_size = 8
+grid_size = 11
 grid_length = 10
 grid_height = 5
 
 
-def connectivity_matrix(grid_size: int, params: dict):
+def connectivity_matrix(grid_size: int, spring_stiffness: float = 1, damping_coefficient: float = 1):
+
     n = grid_size**2 + (grid_size - 1) ** 2
     top_edge = [i for i in range(grid_size)]
     bottom_edge = [n - grid_size + i for i in range(grid_size)]
@@ -24,10 +25,10 @@ def connectivity_matrix(grid_size: int, params: dict):
     # inner grid connections
     for i in range(n):
         if i not in fixed_nodes:
-            connections.append([i, i - grid_size, params["k"], params["c"]])
-            connections.append([i, i - grid_size + 1, params["k"], params["c"]])
-            connections.append([i, i + grid_size - 1, params["k"], params["c"]])
-            connections.append([i, i + grid_size, params["k"], params["c"]])
+            connections.append([i, i - grid_size, spring_stiffness, damping_coefficient])
+            connections.append([i, i - grid_size + 1, spring_stiffness, damping_coefficient])
+            connections.append([i, i + grid_size - 1, spring_stiffness, damping_coefficient])
+            connections.append([i, i + grid_size, spring_stiffness, damping_coefficient])
 
     # Filtering duplicates
     filtered_connections = []
@@ -38,9 +39,12 @@ def connectivity_matrix(grid_size: int, params: dict):
             and link not in filtered_connections
         ):
             filtered_connections.append(link)
-    print(
-        f"Filtered connections from {len(connections)} down to {len(filtered_connections)}"
-    )
+
+    #adding extra information required for kitefem
+    for connection in filtered_connections:
+        connection.append(0) #rest length
+        connection.append("default") #springtype  
+
     return filtered_connections, fixed_nodes
 
 
@@ -92,60 +96,28 @@ def initial_conditions(
     return conditions
 
 
-# dictionary of required parameters
-params = {
-    # model parameters
-    "n": 10,  # [-]       number of particles
-    "k_t": 1,  # [N/m]     spring stiffness
-    "c": 1,  # [N s/m] damping coefficient
-    "L": 10,  # [m]       tether length
-    "m_block": 100,  # [kg]     mass attached to end of tether
-    "rho_tether": 0.1,  # [kg/m]    mass density tether
-    # simulation settings
-    "dt": 0.1,  # [s]       simulation timestep
-    "t_steps": 1000,  # [-]      number of simulated time steps
-    "abs_tol": 1e-50,  # [m/s]     absolute error tolerance iterative solver
-    "rel_tol": 1e-5,  # [-]       relative error tolerance iterative solver
-    "max_iter": 1e5,  # [-]       maximum number of iterations
-    # physical parameters
-    "g": 9.807,  # [m/s^2]   gravitational acceleration
-    "v_w": [5, 0, 0],  # [m/s]     wind velocity vector
-    "rho": 1.225,  # [kg/ m3]  air density
-    "c_d_bridle": 1.05,  # [-]       drag-coefficient of bridles
-    "d_bridle": 0.02,  # [m]       diameter of bridle lines
-}
 
-# calculated parameters
-params["l0"] = 0  # np.sqrt( 2 * (grid_length/(grid_size-1))**2)
-params["m_segment"] = 1
-params["k"] = params["k_t"] * (params["n"] - 1)  # segment stiffness
-params["n"] = grid_size**2 + (grid_size - 1) ** 2
+
 
 
 # instantiate connectivity matrix and initial conditions array
-connections, f_nodes = connectivity_matrix(grid_size, params)
+connections, f_nodes = connectivity_matrix(grid_size)
 init_cond = initial_conditions(
-    grid_size, params["m_segment"], f_nodes, grid_height, grid_length
+    g_size= grid_size, m_segment = 1, fixed_nodes= f_nodes, g_h= grid_height, g_l= grid_length
 )
 
-for connection in connections:
-    connection.append(params["l0"])
-    connection.append("default")  
-
-spring_matrix = connections
-initial_conditions = init_cond
-
 # Create FEM structure and solve
-SaddleForm = FEM_structure(initial_conditions, spring_matrix)
+SaddleForm = FEM_structure(init_cond, connections)
 ax1, fig1 = plot_structure(SaddleForm, plot_displacements=False)
 SaddleForm.solve(
     fe=None,
-    max_iterations=1000,
+    max_iterations=10000,
     tolerance=0.1,
     step_limit=0.25,
     relax_init=0.25,
     relax_update=0.95,
     k_update=10,
+    convergence_criteria="residual"
 )
 
 
